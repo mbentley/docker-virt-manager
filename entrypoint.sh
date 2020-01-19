b4bd8c42ecf7d0202784e93401704d1661e8594d
Matt Bentley <mbentley@mbentley.net>
2020-01-19 06:56:46 -0500

    Updated entrypoint for machine-id
    See https://wiki.debian.org/MachineId
    
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 # start ssh agent
 eval "$(ssh-agent)"
 
+# generate new machine-id because virt-manager wants this to exist
+dbus-uuidgen --ensure=/etc/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
+
 # execute command
 exec "${@}"
